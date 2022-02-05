6a8a711a63b76c68b5d7068095dbc7d0c8dc85c2
aongtosan <aongtosan@gmail.com>
2022-02-05 20:32:30 +0700

    update-camera-look

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Transform playerPosition;
+    Vector3 cameraOffset;
+    void Start()
+    {
+        cameraOffset = transform.position - playerPosition.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, transform.position.y, playerPosition.position.z+cameraOffset.z);
+    }
+}
