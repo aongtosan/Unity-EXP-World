dcdff60e814e69924ce82b96475f5921edb1c277
aongtosan <aongtosan@gmail.com>
2022-02-06 15:32:06 +0700

    update-auto-open/close chest

--- Assets/Resources/Scripts/CrateController.cs
+++ Assets/Resources/Scripts/CrateController.cs
@@ -9,14 +9,19 @@ public class CrateController : MonoBehaviour
     [SerializeField]
     Transform playerPosition;
     BoxCollider box;
+    GameObject crate;
+    Animator crate_animate;
     void Awake()
     {
         box = GetComponent<BoxCollider>();
         GameObject area = new GameObject("chest-area");
+        crate = transform.GetChild(0).gameObject;
+        crate_animate = crate.GetComponent<Animator>();
         area.transform.parent = transform;
         area.transform.localPosition = Vector3.zero;
         area.transform.rotation = new Quaternion(0, 0, 0, 0);
         area.drawRectangle(box.bounds.size.x,box.bounds.size.z, 0.1f);
+
     }
 
     // Update is called once per frame
@@ -26,9 +31,11 @@ public class CrateController : MonoBehaviour
        if((playerPosition.position-transform.position).magnitude< box.bounds.size.x )
         {
             area.GetComponent<LineRenderer>().enabled = true;
+            crate_animate.SetBool("Open", true);
         }
         else
         {
+            crate_animate.SetBool("Open", false);
             area.GetComponent<LineRenderer>().enabled = false;
         }
            
